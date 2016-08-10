de6dfbb3b33b9cda060a25c92774c71abfbf9bc5
Abey Onalaja <abey.onalaja@gmail.com>
2016-08-10 11:42:10 +0100

    Add charts for pressure and humidity

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 
@@ -14,17 +14,22 @@ class WeatherList extends Component {
   renderWeather( cityData ) {
 
     const name = cityData.city.name;
+    const colors = ["green", "red", "blue"];
     const temps = cityData.list.map( weather => weather.main.temp );
+    const pressures = cityData.list.map( weather => weather.main.pressure );
+    const humidities = cityData.list.map( weather => weather.main.humidity );
+
     return (
       <tr key={ name }>
         <td>{ name }</td>
         <td>
-          <Sparklines
-             height={120}
-             width={180}
-             data={temps} >
-            <SparklinesLine color="green" />
-          </Sparklines>
+          <Chart data={ temps } color={ "blue" } />
+        </td>
+        <td>
+          <Chart data={ pressures } color={ "purple" } />
+        </td>
+        <td>
+          <Chart data={ humidities } color={ "green" } />
         </td>
       </tr>
     );
